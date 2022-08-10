cdd13be10638020dba20d5971e7a0d7062b98baf
Marcos-An <loseonjr@gmail.com>
2022-08-10 17:11:40 -0400

    add PORT

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
